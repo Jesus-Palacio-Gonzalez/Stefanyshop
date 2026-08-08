fa401d2a78ff9df9afb8708a1c49148c35e49c83
Jesuspalacio <jesuspalaciogonzalez@gmail.com>
2026-08-08 17:48:06 -0500

    feat: update StefyShop project configuration

---
+++ dist/StefyShop.9e4250b7.js
@@ -0,0 +1,2 @@
+const BRAND_LINKS={ardysslife:"/ArdyssLife/index.html",artesanias:"/ArtesaniasCJ/index.html",diosa:"/DiosaInterior/index.html"};document.querySelectorAll(".panel").forEach(e=>{let t=e.dataset.brand;BRAND_LINKS[t]&&e.setAttribute("href",BRAND_LINKS[t])});
+//# sourceMappingURL=StefyShop.9e4250b7.js.map
