a766f1f10f9674a0034937db6b9e3105a00714d3
Jesuspalacio <jesuspalaciogonzalez@gmail.com>
2026-08-08 16:59:04 -0500

    v 1.0

---
+++ dist/StefyShop.49dc845a.js
@@ -0,0 +1,2 @@
+const BRAND_LINKS={ardysslife:"/ArdyssLife/index.html",artesanias:"/ArtesaniasCJ/index.html",diosa:"/DiosaInterior/index.html"};document.querySelectorAll(".panel").forEach(e=>{let t=e.dataset.brand;BRAND_LINKS[t]&&e.setAttribute("href",BRAND_LINKS[t])});
+//# sourceMappingURL=StefyShop.49dc845a.js.map
